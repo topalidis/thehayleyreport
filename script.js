d10e4253390b77a3d21b1d653290adec5aa4f679
Nick <nicholas.gelyana@gmail.com>
2022-06-30 11:17:55 -0500

    Fixed scaling & colors

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.addEventListener("scroll", function(){
   if(window.scrollY==0){
-    document.querySelector("header").style.background = "rgba(0,0,0,0)";
+    document.querySelector("header").style.background = "#eff0ed";
     document.querySelector("header").style.color = "var(--header-color)";
   } else {
-    document.querySelector("header").style.background = "rgba(239, 240, 237, .95)";
+    document.querySelector("header").style.background = "#eff0ed";
     document.querySelector("header").style.color = "#000000";
   }
 });
